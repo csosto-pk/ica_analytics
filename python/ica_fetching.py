cf29851721202093393cf97b9e304d786c0e3433
csosto-pk <pankab@gmail.com>
2021-04-15 00:12:07 -0400

    More logic to the fetching to work better and use count and start lines.

--- python/ica_fetching.py
+++ python/ica_fetching.py
@@ -5,9 +5,9 @@
 # intermediate CA certs. 
 
 # Run as 
-#  python3 ica_fetching.py ../data/example2.json --num_servers 5 --server_file ../data/umbrella-top-1m.csv_4-5-2021.csv 
+#  python3 ica_fetching.py ../data/example100.json --server_file ../data/umbrella-top-1m.csv_4-5-2021.csv --num_servers 100 --line_start 10
 # or to just update the empty ICAs in the JSON file 
-# python3 ica_fetching.py ../data/example2.json  
+# python3 ica_fetching.py ../data/example2.json --num_servers 100 --line_start 10
 
 
 import sys, os.path
@@ -18,6 +18,7 @@ import argparse
 from pprint import pprint
 
 PROGRESS_PRINT_CTR = 10 # To be used to print progress dots as the ICAs are being fetched.
+WRITE_CTR = 5 # To be used to write to file periodically so we don't lose data in case of failure.
 
 def get_certificate_chain(host):
     """
@@ -60,12 +61,15 @@ paramparser.add_argument('--server_file', nargs='?',
 			help="File containing the servers. If empty, we will only update the server_ICA_file JSON objects for the servers that had no ICA information stored for them.")
 paramparser.add_argument("--num_servers", type=int, nargs='?', default=1000000,
 			help="Number of servers to fetch ICAs for. Default is 1M.")
+paramparser.add_argument("--line_start", type=int, nargs='?', default=1,
+			help="Starting line for server to start fetching ICAs for. Default is 1.")
 args = paramparser.parse_args()
 
 '''# Pring input arguments for testing 
 print(args.server_ICA_file)
 print(args.num_servers)
 print(args.server_file)
+print(args.line_start)
 '''
 
 def ica_list_to_json(ica_certs):
@@ -82,7 +86,7 @@ def ica_list_to_json(ica_certs):
   return json_s[:len(json_s)-1] # Remove uncessary comma that would break json
 
 # Starting 
-rows = args.num_servers # counter for the number of server to fetch ICAs for
+srv_cnt = args.num_servers # counter for the number of server to fetch ICAs for
 if args.server_file: # If passed as input parameter, parse the servers file and fetch ICAs
   print ("Fetching ICAs for", args.num_servers, "servers...")
   sfile = args.server_file # the file that contains the servers. 
@@ -92,51 +96,63 @@ if args.server_file: # If passed as input parameter, parse the servers file and 
   else:
     with open(sfile) as csv_file: # Open and read the file with the servers.
         csv_reader = csv.reader(csv_file, delimiter=',')
+        jsonFile = open(args.server_ICA_file, "w").close() # Start with empty file. TODO: Need to throw error if it already exists.
         for row in csv_reader: 
             if (int(row[0]) % PROGRESS_PRINT_CTR == 0): # For every PROGRESS_PRINT_CTR servers
               print(".", end ="", flush=True) # print progress dot 
-            if int(row[0]) > rows: # Exit it you processed the number of servers required
+            if int(row[0]) - args.line_start > srv_cnt: # Exit it you processed the number of servers required
                break
-            else: 
-               json_str += """ {"Server": \"""" + row[1] +"""\", """
+            if (not int(row[0]) < args.line_start): 
+               json_str += """ {"Id": \"""" + row[0] +"""\", """
+               json_str += """ "Server": \"""" + row[1] +"""\", """
                #print("server:", row[1], end =", ")
                certs = get_certificate_chain(row[1]) # Fetch the ICA cert chain from the server
                #pprint(certs)
                json_str += """ "ICAS": [""" + ica_list_to_json(certs) + """] },"""
-            #if int(row[0]) < rows: # If it is the last entry, don't add comma in the json
+            #if int(row[0]) < srv_cnt: # If it is the last entry, don't add comma in the json
             #  json_str += json_str + ","
-              #TODO: If certs are empty, then increase rows++ to get one more entry because this was a fluke.
+              #TODO: If certs are empty, then increase srv_cnt++ to get one more entry because this was a fluke.
+               if (int(row[0]) % WRITE_CTR == 0): # For every PROGRESS_PRINT_CTR servers
+                 jsonFile = open(args.server_ICA_file, "a")
+                 jsonFile.write(json_str)
+                 jsonFile.close()
+                 if int(row[0]) - args.line_start < srv_cnt: # Don't empty the json string in the last iteration,   
+                   json_str = ""        # we will use it to remove the comma to not break json
   print("") # Print new line
   json_str = json_str[:len(json_str)-1] + "]" # Remove uncessary comma that would break json
-  jsonFile = open(args.server_ICA_file, "w")
+  jsonFile = open(args.server_ICA_file, "a")
   jsonFile.write(json_str)
   jsonFile.close()
 
-else: # if only asked to populate the server json, only parse the server entries without any ICAs 
+else: # if only asked to populate the server json, only parse the server entries in the JSON file without any ICAs 
   print ("Populating empty ICA lists in JSON file...")
   jsonFile = open(args.server_ICA_file, "r") # Open the JSON file for reading
   data = json.load(jsonFile) # Read the JSON into the buffer
+  data_orig_len = len(data) # Read the JSON into the buffer
   jsonFile.close() # Close the JSON file
+  updatedf = 0; 
   for sobj in data: 
+      #TODO: if int(row[0]) - args.line_start > srv_cnt: # Exit it you processed the number of servers required
+             #  break
+      #TODO: Add if (not sobj["Id"] < args.line_start): 
       for attr, value in sobj.items():
           #print(attr, value)
           #print("++", sobj["ICAS"]) 
           if attr == 'ICAS' and value == []:
-            print(" Missing ICAs for", sobj["Server"], end =",", flush=True) 
+            print(sobj['Server'], end =" ", flush=True) 
             certs = get_certificate_chain(sobj['Server']) # Fetch the ICA cert chain from the server
             if not certs == []: 
               for i in range(len(certs)): 
                 sobj["ICAS"] = [  json.loads(ica_list_to_json(certs)) ]
-              print(" got updated", end =".", flush=True) 
+              print("U.", end =" ", flush=True) 
+              updatedf = 1
             else: 
-              print(" no luck this time either", end =".", flush=True)
+              print("N.", end =" ", flush=True)
       #print(sobj) 
   ## Save our changes to JSON file  
   print("") # Print new line
-  jsonFile = open(args.server_ICA_file, "w")
-  jsonFile.write(json.dumps(data))
-  jsonFile.close()
-
-
-
+  if updatedf:   # Write only if we got more ICAs
+    jsonFile = open(args.server_ICA_file, "w")
+    jsonFile.write(json.dumps(data))
+    jsonFile.close()
 
