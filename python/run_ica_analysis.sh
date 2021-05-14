23f0bd6d53d5e98218657f3fbf1a8789fef9becc
csosto-pk <pankab@gmail.com>
2021-05-14 00:08:58 -0400

    Run analytics script

---
+++ python/run_ica_analysis.sh
@@ -0,0 +1,27 @@
+# /bin/basj
+
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20200602.json --csv > ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20200707.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20200804.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20200901.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20201006.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20201103.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20201201.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20210101.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20210201.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20210301.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20210401.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/certs_alexa/certs_alexa_20210501.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20200602.json --csv >> ../certs_results.csv 
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20200707.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20200804.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20200901.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20201006.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20201103.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20201201.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20210101.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20210201.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20210301.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20210401.json --csv >> ../certs_results.csv
+python3 ica_analysis.py ../data/censys.io/cisco_umbrella/certs_cisco_20210501.json --csv >> ../certs_results.csv
+
